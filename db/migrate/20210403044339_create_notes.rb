47e4169747bb06fe0d3f24285773168450670cac
Amanda <amandaben21@aol.com>
2021-04-03 00:44:45 -0400

    Create User & Note resource

---
+++ db/migrate/20210403044339_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notes do |t|
+      t.string :subject
+      t.string :date
+      t.string :note_entry
+
+      t.timestamps
+    end
+  end
+end
