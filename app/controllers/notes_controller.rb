8fba4f8acdb5a8ac3bb77065dd547e672bf2edec
Amanda <amandaben21@aol.com>
2021-04-05 16:20:33 -0400

    Added a few notes to what should happen

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,28 @@
 class NotesController < ApplicationController
+
+    def index
+        #who this should be accessible by?
+    end
+
+    def show
+        #see the notes they created
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+        #restrict access to current_user
+    end
+
+    def update
+    end
+
+    def destory
+        #restrict access to current_user
+    end
+
 end
