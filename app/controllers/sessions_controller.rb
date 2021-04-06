86b1a7047f681a80e3475c17097b4a71ca6f04ad
Amanda <amandaben21@aol.com>
2021-04-06 17:17:49 -0400

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+    def new
+        
+    end
+
+    def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update, :create]
 
   get '/signup', to: "users#new"
+  
+  get '/login', to: "session#new"
+  post '/login', to: "session#create" #to process the information
+  
+  get '/logout', to: "session#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
