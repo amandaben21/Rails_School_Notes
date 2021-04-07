2d5b06bae57c46bfd8a3c267e0e78bcf4dd3a48e
Amanda <amandaben21@aol.com>
2021-04-07 00:10:46 -0400

    Show forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,25 @@
 class SessionsController < ApplicationController
 
     def new
-        
+        #login form
+        @user = User.new
     end
 
     def create
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            redirect_to user_path(@user)
+        #logging in
+        @user = User.find_by_username(params[:user][:username])
+        
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to notes_path
         else
             render :new
         end
     end
 
     def destroy
+        #logging out
+        session.clear
+        redirect_to "/"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   resources :subjects
   resources :notes
   resources :users, only: [:show, :edit, :update, :create]
+  
 
   get '/signup', to: "users#new"
+  #post '/signup', to: "users#new"
   
-  get '/login', to: "session#new"
-  post '/login', to: "session#create" #to process the information
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create" #to process the information
   
-  get '/logout', to: "session#destroy"
+  get '/logout', to: "sessions#destroy"
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
