434394a8d68d04ed77172b334eefaf24ff190340
Amanda <amandaben21@aol.com>
2021-04-07 00:06:29 -0400

    Save a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
     
     def new
-        #form to create user
+      
+        @user = User.new
     end
 
     def show
-        #show the notes that the user entered
+        
+        @user = User.find_by_id(params[:id])
     end
 
     def edit
@@ -15,8 +17,21 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def destory
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
