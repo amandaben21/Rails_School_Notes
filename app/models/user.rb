13093dee35d3f0845c55d8a59211ac3ca3c1fdc9
Amanda <amandaben21@aol.com>
2021-04-07 14:14:52 -0400

    Password secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
     has_many :notes
     has_many :subjects, through: :notes
+
+    has_secure_password
     
 
-    validates :username, :email, :password presence: true
+    validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
 end
