91104ff24b3b261d09c273f551d9376acf3d5f43
Amanda <amandaben21@aol.com>
2021-04-07 00:09:25 -0400

    Has validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_many :notes
-    has_many :subjects, through: notes
-    has_secure_password
+    has_many :subjects, through: :notes
+    
+
+    validates :username, :email, :password presence: true
+    validates :username, :email, uniqueness: true
 end
