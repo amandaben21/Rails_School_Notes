236bff9a6076a01828fd7661d2a25be8a6428659
Amanda <amandaben21@aol.com>
2021-04-07 14:08:01 -0400

    Fix logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  resources :subjects
-  resources :notes
-  resources :users, only: [:show, :edit, :update, :create]
+  root "sessions#home"
   
-
   get '/signup', to: "users#new"
-  #post '/signup', to: "users#new"
+  post '/signup', to: "users#create"
   
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create" #to process the information
   
   get '/logout', to: "sessions#destroy"
+
+  resources :subjects
+  resources :notes
+  resources :users
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
