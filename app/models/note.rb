ac9bc26984240e7c19ee674a70a4417b85a695bb
Amanda <amandaben21@aol.com>
2021-04-11 23:51:24 -0400

    accepts nested attributes for subject & override blank

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,8 @@
 class Note < ApplicationRecord
     belongs_to :user
     belongs_to :subject
+
+    validates :lesson_name, :date, :note_entry, presence: true
+
+    accepts_nested_attributes_for :subject, reject_if: proc { |attributes| attributes['name'].blank? }
 end
