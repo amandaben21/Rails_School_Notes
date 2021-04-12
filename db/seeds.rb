19d9fe100cf44fa541a66289bdf6635fc135cc34
Amanda <amandaben21@aol.com>
2021-04-11 23:53:34 -0400

    Seeded few subjects in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Subject.create([{name: 'English'}, {name: 'Math'}, {name: 'History'}, {name: 'Science'}, {name: 'Foreign Language'}])
