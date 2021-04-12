55d0c1a0103ddf52d93620fb7256f2a3361c98bb
Amanda <amandaben21@aol.com>
2021-04-11 23:49:11 -0400

    Added a before action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,12 @@
 class SubjectsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+    
+    def index
+        @subjects = Subject.all
+    end
+
+    def show
+        @subject = Subject.find_by_id(params[:id])
+    end
+
 end
