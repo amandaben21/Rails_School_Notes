3ae671be73bbaff94e7cdb05d7c3b3a6d10ca4ca
Amanda <amandaben21@aol.com>
2021-04-11 23:45:01 -0400

    Put before actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,42 +1,61 @@
 class NotesController < ApplicationController
-
+    
+    before_action :redirect_if_not_logged_in
+    before_action :set_note, only: [:show, :edit, :update]
+    
     def index
-        #who this should be accessible by?
         @notes = current_user.notes
     end
 
-    def show
-        #see the notes they created
-        @note = Note.find_by_id(params[:id])
-    end
-
     def new
+        @note = Note.new
+        @note.build_subject
     end
 
     def create
-        @note = Note.new(note_params)
+        @note = current_user.notes.build(note_params)
+        
         if @note.save
-            redirect_to note_path
+            redirect_to user_path(@note)
         else
             render :new
         end
     end
 
+    def show
+    end
+
     def edit
-        #restrict access to current_user
+        
     end
 
     def update
+        @note.update(note_params)
+        
+        if @note.save
+            redirect_to note_path(@note)
+        else 
+            render :edit
+        end
     end
 
     def destory
-        #restrict access to current_user
+        if @note
+            @note.destory
+            redirect_to note_path
+        else
+            redirect_to note_path(@notes)
+        end
     end
 
     private
     
     def note_params
-        params.require(:note).permit(:lesson_name)
+        params.require(:note).permit(:lesson_name, :date, :note_entry, :subject_id, subject_attributes: [:name])
+    end
+
+    def set_note
+        @note = current_user.notes.find_by_id(params[:id])
     end
 
 end
