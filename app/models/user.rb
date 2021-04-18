9cdf22e0c6682799e32456eb02a68b7e97eb72cd
Amanda <amandaben21@aol.com>
2021-04-18 15:50:22 -0400

    Added commets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
+require 'securerandom'
+
 class User < ApplicationRecord
+    has_many :comments
     has_many :notes
     has_many :subjects, through: :notes
 
     has_secure_password
     
 
-    validates :username, :email, presence: true
+    validates :username, :email, :password, presence: true
     validates :username, :email, uniqueness: true
+
+    def self.find_by_omniauth(auth)
+        self.find_or_create_by(uid: auth[:uid]) do |u|
+            u.username = auth[:info][:name]
+            u.email = auth[:info][:email]
+            u.password = SecureRandom.hex
+        end
+    end
 end
