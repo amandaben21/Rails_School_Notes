6c022893d28c9dafe19640a565dbf29efb11b707
Amanda <amandaben21@aol.com>
2021-04-19 20:14:03 -0400

    change the drop down

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,5 @@ class Comment < ApplicationRecord
     belongs_to :note
 
     validates :content, presence: true
-    #validates :user_id, presence: true
-    #validates :note_id, presence: true
+  
 end
